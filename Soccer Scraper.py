3b0cb1cdda0ff3a535e85d664c66d23daa32ee66
Shapyio <sagievg@gmail.com>
2023-11-27 02:43:49 -0500

    Soccer Scraper can now go through table and extract link from every match that has taken place.

--- Soccer Scraper.py
+++ Soccer Scraper.py
@@ -1,14 +1,21 @@
-import pandas as pd
-from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import requests
 
 url = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60"}
 
-request = Request(url)
-request.add_header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60")
-page = urlopen(request)
-html_content = page.read()
+response = requests.get(url, headers=header)
 
-table = pd.read_html(html_content)
+if response.status_code == 200:
+        html_soup = BeautifulSoup(response.text, 'html.parser')
 
-for x in table:
-        print(x, "\n")
+        table_element = html_soup.find('table')
+        rows = table_element.find_all('tr')
+
+        count = 0
+        for r in rows:
+                count = count + 1
+                columns = r.find_all('td')
+                for c in columns:
+                        if c['data-stat'] == 'match_report' and c.text.strip() == "Match Report" :
+                                print(count, ". ", c.find('a')['href'])
